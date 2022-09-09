9e7057621b940ffadf5fbb02a9b114552614cdd1
Baileigh <baileighbrown@Baileighs-MacBook-Pro.local>
2022-09-09 14:00:38 -0400

    background change

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
+
+
+
 
 //remove active class from menu iems menu
 const changeActiveItem = () => {
@@ -90,12 +97,12 @@ const removeSizeSelector = () => {
 }
 
 fontSizes.forEach(size => {
+ 
+    size.addEventListener('click', () => {   
     removeSizeSelector();
     let fontSize;
-size.classList.toggle('active');
+    size.classList.toggle('active');
 
-
-    size.addEventListener('click', () => {
         if(size.classList.contains('font-size-1')){
             fontSize = '10px';
             root.style.setProperty('-----sticky-top-left', '5.4rem');
@@ -122,3 +129,82 @@ size.classList.toggle('active');
     })
    
 })
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+//change primary colors 
+colorPalette.forEach(color => {
+    color.addEventListener('click',()=>{
+        let primaryHue;
+        // remove active class from colors
+        changeActiveColorClass();
+        if(color.classList.contains('color-1')){
+            primaryHue = 252;
+        
+        } else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        }else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }  
+        color.classList.add('active');
+
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    }) 
+})
+
+//baackground theme color
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () =>{
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+Bg1.addEventListener('click', ()=>{
+    //add active class
+    Bg1.classList.add('active');
+    //remove active class from others
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+    window.location.reload();
+});
+
+Bg2.addEventListener('click', ()=>{
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    //add active class
+    Bg2.classList.add('active');
+    //remove active class from others
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+});
+
+//BG3
+Bg3.addEventListener('click', ()=>{
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+//add active class
+Bg3.classList.add('active');
+//remove active class from others
+Bg1.classList.remove('active');
+Bg2.classList.remove('active');
+changeBG();
+})
+//end
